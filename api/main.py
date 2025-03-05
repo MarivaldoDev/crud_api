4b3c3e281b2c07d67554593cb864f4b1522b3416
MarivaldoDev <pedromarivaldo10@gmail.com>
2025-03-05 10:38:58 -0300

    adicionando template

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from api.database.models.schemas import Message
 from http import HTTPStatus
 from api.database.config_db import SessionLocal, engine, Base
 from api.routers import users, auth, todos
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
 
 
 Base.metadata.create_all(bind=engine)
@@ -21,11 +23,13 @@ app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(todos.router)
 
+BASE_DIR = Path(__file__).parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 # PÁGINA INICIAL
 @app.get("/",
     status_code=HTTPStatus.OK,
     response_model=Message
 )
-def home():
-    return {"message": "A API tá no ar. Acesse /docs para ver a documentação"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
